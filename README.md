5835164ea0b999db88ccd55276bc5d61279a6608
Ryan <ryanthephan@gmail.com>
2023-02-20 18:50:06 -0800

    adding in my part for a3

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Main-OneStopStock
+Welcome to One Stop Stock, your one stop for all your store stock needs.
--- main/mainapp/urls.py
+++ main/mainapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('tristen_a3p3/', views.tristen_a3p3, name='tristena3p3'),
+    path('ryana3p3/', views.ryana3p3, name='ryana3p3'),
 ]
