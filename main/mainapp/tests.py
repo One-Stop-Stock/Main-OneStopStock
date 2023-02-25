9f7ccad93268b8eaf5316310cafd3562d30c5135
tetsuobbb <tristenpiston@hotmail.com>
2023-02-24 19:27:21 -0800

    More selenium testing, installed webdriver-manager library for selenium

--- main/mainapp/tests.py
+++ main/mainapp/tests.py
@@ -1,8 +1,16 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+import time
 
 class SeleniumTest(LiveServerTestCase):
     def testhomepage(self):
-        driver = webdriver.Edge()
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
         driver.get('https://google.com')
+
+        time.sleep(5)
+
+        driver.quit()
