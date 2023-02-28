980fef09c32c560296eeb475c7206714cf65044e
Ryan <ryanthephan@gmail.com>
2023-02-28 08:59:38 -0800

    Target Demo

--- main/mainapp/views.py
+++ main/mainapp/views.py
@@ -26,17 +26,18 @@ def ryana3p3(request, *args, **kwargs):
 def getResult(input):
     #Change driver and driver location
     driver = webdriver.Chrome("C://Users/cubix/Downloads/chromedriver.exe")
-    walmartLink = "https://www.walmart.com/search?q="
+    walmartLink = "https://www.target.com/s?searchTerm="
     itemSearch = input
     link = walmartLink + itemSearch
     driver.get(link)
-    time.sleep(2)
+    time.sleep(4)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.close()
 
     content = list()
     k = {}
 
+    """ #Walmart Test
     #finding all the items on the page
     try:
         item = soup.find_all("span", {"class" : "w_V_DM"})
@@ -51,6 +52,33 @@ def getResult(input):
             k["Item{}".format(i + 1)] = None
     content.append(k)
     k = {}
+    """
+    
+    try:
+        item = soup.find_all("div", {"class": "Truncate-sc-10p6c43-0 flAIvs"})
+    except:
+        item = None
+    
+    
+    '''
+    #Only get the first 4 items
+    for i in range (0,4):
+        try:
+            k["Item{}".format(i + 1)] = item[i].find("span", {"data-automation-id": "product-title"}).text.replace("\n","")
+        except:
+            k["Item{}".format(i + 1)] = None
+    content.append(k)
+    k = {}
+    '''
+
+    for i in range(0, 4):
+        try:
+            k["Item{}".format(i + 1)] = item[i].find("a", {"data-test": "product-title"}).text.replace("\n", "")
+        except:
+            k["Item{}".format(i + 1)] = None
+        content.append(k)
+        k = {}
+
 
     return (content)
 
