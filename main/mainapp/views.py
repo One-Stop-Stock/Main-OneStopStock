016724494efbda33a3a42107d47450b6eeeffd89
tetsuobbb <tristenpiston@hotmail.com>
2023-03-01 16:11:41 -0800

    a4 work...

--- main/mainapp/views.py
+++ main/mainapp/views.py
@@ -25,7 +25,7 @@ def ryana3p3(request, *args, **kwargs):
 
 def getResult(input):
     #Change driver and driver location
-    driver = webdriver.Edge("/Users/tbizzy/Desktop/Main-OneStopStock/main/edgedriver_mac64/msedgedriver")
+    driver = 
     walmartLink = "https://www.target.com/s?searchTerm="
     itemSearch = input
     link = walmartLink + itemSearch
